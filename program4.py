80b580712ddaf5596b326a9490a136fb2a8ba5b2
DharmaTeja <dharmateja9398@gmail.com>
2023-09-21 16:17:24 +0530

    modified program 4

--- program4.py
+++ program4.py
@@ -1,8 +1,9 @@
 import numpy as np
+import statistics as stats
 numbers = [1,2,3,4,5,6,6]
 n= len(numbers)
 print('mean of the entered data :',np.mean(numbers))
 print('median of the entered data:', np.median(numbers))
-print('Mode of the entered data:', np.mode(numbers))
+print('Mode of the entered data:', stats.mode(numbers))
 print('variance of the entered data',np.var(numbers))
 print('variance of the entered data',np.std(numbers))

ee55f353caba11500713e6bbb170d5c9ef078e86
DharmaTeja <dharmateja9398@gmail.com>
2023-09-21 15:45:52 +0530

    program 1 added

--- Program1.py
+++ Program1.py
@@ -1,10 +1,10 @@
 #1. write a program for calculating the mean,median,mode standard deviation and variance without using predefined methods from numpy.
+import math
 
 def mean(numbers,n):
     return sum(numbers)/n
 
 def median(numbers,n):
-    import math
     if(n%2==0):
         median = (numbers[math.floor(n/2)]+numbers[math.floor(n/2)+1])/2
     else:
@@ -29,7 +29,6 @@ def mode(numbers):
 def variance(numbers,mean):
     variance =0
     for number in numbers:
-        import math
         variance+= math.pow((number - mean),2)
     return variance/len(numbers)
         
@@ -42,5 +41,4 @@ print('mean of the entered data :',mean)
 print('median of the entered data:', median(sorted(numbers),n))
 print('Mode of the entered data:', mode(sorted(numbers)))
 print('variance of the entered data',variance)
-import math
 print('variance of the entered data',math.sqrt(variance))
